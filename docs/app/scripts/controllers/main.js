89848fe64f9c0fafc19c39d151fd8de4a29aa894
Les Hazlewood <les@hazlewood.com>
2014-03-07 08:08:15 -0800

    Added yeoman-based angular-bootstrap to docs.  Mostly testing - still TBD if we'll use this longer term.

---
+++ docs/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('docsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
