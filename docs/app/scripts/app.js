82e563468cdd5b71fe16f0e0f68709c0752f2647
Les Hazlewood <les@hazlewood.com>
2014-03-07 14:53:26 -0800

    Minor docs changes - testing L&F and HTML5 URL state fix

--- docs/app/scripts/app.js
+++ docs/app/scripts/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('docsApp', [
+  'ui.bootstrap',
   'ngCookies',
   'ngResource',
   'ngSanitize',
   'ngRoute'
 ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true); //make our app's content SEO friendly!
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
