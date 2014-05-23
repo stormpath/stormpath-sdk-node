b8ffc0470dc5723f4f83f1857674fca1705293fb
Dmitriy Shekhovtsov <valorkin@gmail.com>
2014-05-23 19:17:35 +0300

    update test\common

--- test/common.js
+++ test/common.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 //_.extend(process.env, require('./test.env'));
 
 var chai = require("chai");
+var nock = require('nock');
 var sinon = require("sinon");
 var assert = require('chai').assert;
 var should = require("chai").should();
@@ -13,9 +14,16 @@ var sinonChai = require("sinon-chai");
 var Stormpath = require('../lib');
 chai.use(sinonChai);
 
+function u(){}
+u.BASE_URL = 'https://api.stormpath.com/v1';
+/** adds '/v1' to relative URL, to work with nock request mocker  */
+u.v1 = function(s){return '/v1' + s;};
+
 module.exports = {
   _: _,
+  u: u,
   chai: chai,
+  nock: nock,
   sinon: sinon,
   assert: assert,
   expect: chai.expect,
