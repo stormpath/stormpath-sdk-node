c0542e85fe743306c56f9b1db8d517d9c88203dd
Robert <robert@stormpath.com>
2015-05-27 14:45:41 -0700

    configurable api key options, with benchmarks

---
+++ benchmark/api-key-encryption.js
@@ -0,0 +1,139 @@
+var stormpath = require('../');
+var uuid = require('node-uuid');
+var base64 = require('../lib/utils').base64;
+var Benchmark = require('benchmark');
+
+console.log('Construct Clients');
+
+// First client is used to create our dummy account
+
+var client1 = new stormpath.Client({
+  apiKey: new stormpath.ApiKey(
+    process.env['STORMPATH_API_KEY_ID'],
+    process.env['STORMPATH_API_KEY_SECRET']
+  )
+});
+
+// Second client will use default options, Api Key encryption will be enabled
+
+var client2 = new stormpath.Client({
+  apiKey: new stormpath.ApiKey(
+    process.env['STORMPATH_API_KEY_ID'],
+    process.env['STORMPATH_API_KEY_SECRET']
+  )
+});
+
+// Third client will disable api key encryption
+
+var client3 = new stormpath.Client({
+  apiKey: new stormpath.ApiKey(
+    process.env['STORMPATH_API_KEY_ID'],
+    process.env['STORMPATH_API_KEY_SECRET']
+  ),
+  apiKeyEncryptionOptions: {
+    encryptSecret: false
+  }
+});
+
+var account;
+
+
+
+var suite = new Benchmark.Suite('api auth')
+  .on('start', function() {
+    console.log('Begin benchmarks');
+  })
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('\nFastest is ' + this.filter('fastest').pluck('name')+'\n');
+    account.delete(function(err){
+      if(err){ throw err; }
+      console.log('Deleted account');
+    });
+  });
+
+function invoke(app,apiKey,cb){
+  app.authenticateApiRequest({
+    request:{
+      method: 'GET',
+      url: '/',
+      headers:{
+        'authorization': 'Basic '+ base64.encode(apiKey.id+':'+apiKey.secret)
+      }
+    }
+  },cb);
+}
+
+
+client1.getApplication(process.env['STORMPATH_APP_HREF'],function(err,app) {
+
+  if(err){ throw err; }
+
+  console.log('Create test account');
+
+  app.createAccount({
+    email: uuid()+'@stormpath.com',
+    password: uuid() + 'ABC1',
+    givenName: uuid(),
+    surname: uuid()
+  },function(err,result){
+
+    if(err){ throw err; }
+
+    account = result;
+
+    account.createApiKey(function(err,apiKey){
+
+      client2.getApplication(process.env['STORMPATH_APP_HREF'],function(err,app2) {
+
+        if(err){ throw err; }
+
+        // Make an initial invocation to warm the cache
+
+        invoke(app2,apiKey,function(err){
+
+          if(err){ throw err; }
+
+          suite.add('With encryption ENABLED (default options)      ', {
+            defer: true,
+            fn: function(deferred){
+              invoke(app2,apiKey,function(err){
+                if(err){
+                  throw err;
+                }else{
+                  deferred.resolve();
+                }
+              });
+            }
+          });
+        });
+
+        client3.getApplication(process.env['STORMPATH_APP_HREF'],function(err,app3) {
+
+          if(err){ throw err; }
+
+          // Make an initial invocation to warm the cache
+
+          invoke(app3,apiKey,function(err){
+            if(err){ throw err; }
+            suite.add('With encryption DISABLED (encryptSecret: false)', {
+              defer: true,
+              fn: function(deferred){
+                invoke(app3,apiKey,function(err){
+                  if(err){
+                    throw err;
+                  }else{
+                    deferred.resolve();
+                  }
+                });
+              }
+            });
+            suite.run({ 'async': true });
+          });
+        });
+      });
+    });
+  });
+});
