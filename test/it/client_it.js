63875aa511b0d89be80f15473ff1e32ada66e63a
Randall Degges <r@rdegges.com>
2015-08-20 00:19:15 -0700

    Adding tests to assert that Client.mergeRemoteConfig loads socialProviders properly into the config!

--- test/it/client_it.js
+++ test/it/client_it.js
@@ -1,7 +1,13 @@
+'use strict';
+
+var assert = require('assert');
+
+var async = require('async');
+var uuid = require('uuid');
+
 var common = require('../common');
 var stormpath = common.Stormpath;
 var helpers = require('./helpers');
-var assert = common.assert;
 
 describe('Client', function() {
   describe('creation', function() {
@@ -13,6 +19,91 @@ describe('Client', function() {
           new stormpath.Client({ apiKey:apiKey });
         };
         done();
+      });
+    });
+
+    it('shoud propertly load socialProviders onto the configuration object', function(done) {
+      helpers.getClient(function(client) {
+        var applicationData = { name: uuid.v4() };
+        var application, directory;
+
+        async.series([
+          // First, we'll create a new Stormpath Application.
+          function(cb) {
+            client.createApplication(applicationData, { createDirectory: true }, function(err, app) {
+              if (err) {
+                return cb(err);
+              }
+
+              // Update the Client config, so that when we move onto the mapping
+              // step we'll be able to merge in the config!
+              client.config.application = {
+                href: app.href
+              };
+
+              application = app;
+              cb();
+            });
+          },
+
+          // Next, we'll create a Facebook Directory.
+          function(cb) {
+            var directoryData = {
+              name: uuid.v4(),
+              provider: {
+                clientId: 'xxx',
+                clientSecret: 'yyy',
+                providerId: 'facebook'
+              }
+            };
+
+            client.createDirectory(directoryData, function(err, dir) {
+              if (err) {
+                return cb(err);
+              }
+
+              directory = dir;
+              cb();
+            });
+          },
+
+          // Finally, we'll map our newly created Directory to our Application.
+          function(cb) {
+            var mappingData = {
+              application: {
+                href: application.href
+              },
+              accountStore: {
+                href: directory.href
+              },
+              isDefaultAccountStore: false,
+              isDefaultGroupStore: false
+            };
+
+            application.createAccountStoreMapping(mappingData, function(err) {
+              if (err) {
+                return cb(err);
+              }
+
+              cb();
+            });
+          }
+        ], function(err) {
+          if (err) {
+            return done(err);
+          }
+
+          // Now, we'll run our actual Client.mergeRemoteConfig method to
+          // attempt to grab all social providers.
+          client.mergeRemoteConfig(function(err) {
+            if (err) {
+              return done(err);
+            }
+
+            assert.equal(client.config.socialProviders.length, 1);
+            done();
+          });
+        });
       });
     });
 
