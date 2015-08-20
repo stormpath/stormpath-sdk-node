490d1badccb40f463ae0df5dc67f8039f533c0d1
Randall Degges <r@rdegges.com>
2015-08-19 23:36:39 -0700

    Fixing styles. Code quality++.

--- test/it/client_it.js
+++ test/it/client_it.js
@@ -3,38 +3,43 @@ var stormpath = common.Stormpath;
 var helpers = require('./helpers');
 var assert = common.assert;
 
-describe('Client',function(){
-  describe('creation',function(){
+describe('Client', function() {
+  describe('creation', function() {
     var createClientFn;
-    before(function(done){
-      helpers.loadApiKey(function(apiKey){
-        createClientFn = function createClientFn(){
-          new stormpath.Client({apiKey:apiKey});
+
+    before(function(done) {
+      helpers.loadApiKey(function(apiKey) {
+        createClientFn = function createClientFn() {
+          new stormpath.Client({ apiKey:apiKey });
         };
         done();
       });
     });
-    it('should not throw',function(){
+
+    it('should not throw', function() {
       assert.doesNotThrow(createClientFn);
     });
   });
-  describe('getCurrentTenant',function(){
+
+  describe('getCurrentTenant', function() {
     var result;
     var Tenant = require('../../lib/resource/Tenant');
-    before(function(done){
-      helpers.getClient(function(client){
-        client.getCurrentTenant(function(err,tenant){
-          result = [err,tenant];
+
+    before(function(done) {
+      helpers.getClient(function(client) {
+        client.getCurrentTenant(function(err, tenant) {
+          result = [err, tenant];
           done();
         });
       });
     });
-    it('should not err',function(){
-      assert.equal(result[0],null);
-    });
-    it('should return a tenant instance',function(){
-      assert.instanceOf(result[1],Tenant);
+
+    it('should not err', function() {
+      assert.equal(result[0], null);
     });
 
+    it('should return a tenant instance', function() {
+      assert.instanceOf(result[1], Tenant);
+    });
   });
 });
