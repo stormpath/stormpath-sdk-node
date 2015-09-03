e9cc1e20f796e5848351da379f63f726db318d72
Randall Degges <r@rdegges.com>
2015-09-03 16:44:56 -0700

    DEBUG.

--- test/it/account_it.js
+++ test/it/account_it.js
@@ -2,6 +2,8 @@
 
 var assert = require('assert');
 
+var uuid = require('uuid');
+
 var helpers = require('./helpers');
 
 var Account = require('../../lib/resource/Account');
@@ -14,10 +16,10 @@ describe('Account', function() {
     helpers.getClient(function(_client) {
       client = _client;
 
-      console.log('CREATED CLIENT!', client);
-
-      client.createDirectory({ name: helpers.uniqId() }, function(err, _directory) {
+      client.createDirectory({ name: uuid.v4() }, function(err, _directory) {
         directory = _directory;
+
+        console.log('CREATED DIRECTORY!', directory);
 
         directory.createAccount(helpers.fakeAccount(), function(err,_account) {
           account = _account;
