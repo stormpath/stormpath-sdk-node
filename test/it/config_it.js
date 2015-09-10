2888db9dee47526f915480d5fca72afb0594f769
Randall Degges <r@rdegges.com>
2015-09-09 17:30:51 -0700

    Adding more tests for Config.
    Asserting that the apiKey and application config objects are properly generated
    via environment variables.

--- test/it/config_it.js
+++ test/it/config_it.js
@@ -25,5 +25,39 @@ describe('Config', function() {
       process.env.HOME = home;
       done();
     });
+
+    it('should generate the appropriate apikey options when environment variables are supplied', function(done) {
+      var oldApiKeyId = process.env.STORMPATH_CLIENT_APIKEY_ID;
+      var oldApiKeySecret = process.env.STORMPATH_CLIENT_APIKEY_SECRET;
+
+      process.env.STORMPATH_CLIENT_APIKEY_ID = 'xxx';
+      process.env.STORMPATH_CLIENT_APIKEY_SECRET = 'yyy';
+
+      var config = new Config();
+      assert.equal(config.client.apiKey.id, 'xxx');
+      assert.equal(config.client.apiKey.secret, 'yyy');
+
+      process.env.STORMPATH_CLIENT_APIKEY_ID = oldApiKeyId;
+      process.env.STORMPATH_CLIENT_APIKEY_SECRET = oldApiKeySecret;
+
+      done();
+    });
+
+    it('should generate the appropriate application options when environment variables are supplied', function(done) {
+      var oldApplicationHref = process.env.STORMPATH_APPLICATION_HREF;
+      var oldApplicationName = process.env.STORMPATH_APPLICATION_NAME;
+
+      process.env.STORMPATH_APPLICATION_HREF = 'xxx';
+      process.env.STORMPATH_APPLICATION_NAME = 'yyy';
+
+      var config = new Config();
+      assert.equal(config.application.href, 'xxx');
+      assert.equal(config.application.name, 'yyy');
+
+      process.env.STORMPATH_APPLICATION_HREF = oldApplicationHref;
+      process.env.STORMPATH_APPLICATION_NAME = oldApplicationName;
+
+      done();
+    });
   });
 });
