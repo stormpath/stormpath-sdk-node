34de04a4dad6299dacb0471729753207b52f5281
Robin Orheden <orhedenr@gmail.com>
2015-10-23 19:37:29 +0200

    add support for id site token authenticator

---
+++ test/it/oauth_id_site_token_grant_it.js
@@ -0,0 +1,32 @@
+
+var common = require('../common');
+var helpers = require('./helpers');
+var assert = common.assert;
+
+var stormpath = require('../../');
+
+describe('OAuthIdSiteTokenGrantAuthenticator',function(){
+  var application;
+  var newAccount = helpers.fakeAccount();
+
+  before(function(done){
+    helpers.createApplication(function(err,app){
+      application = app;
+      application.createAccount(newAccount,done);
+    });
+  });
+
+  after(function(done){
+    application.delete(done);
+  });
+
+  it('should be constructable with new operator',function(){
+    var authenticator = new stormpath.OAuthIdSiteTokenGrantAuthenticator(application);
+    assert.instanceOf(authenticator, stormpath.OAuthIdSiteTokenGrantAuthenticator);
+  });
+
+  it('should be constructable without new operator',function(){
+    var authenticator = stormpath.OAuthIdSiteTokenGrantAuthenticator(application);
+    assert.instanceOf(authenticator, stormpath.OAuthIdSiteTokenGrantAuthenticator);
+  });
+});
