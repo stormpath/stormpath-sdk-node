2d111450793e90b5869cca7934efa6d8f4d31745
mdeggies <michele@stormpath.com>
2016-09-15 21:35:25 -0700

    Updated test

--- test/sp.resource.apikey_test.js
+++ test/sp.resource.apikey_test.js
@@ -7,11 +7,12 @@ var uuid = require('uuid');
 var ApiKey = require('../lib/resource/ApiKey');
 var DataStore = require('../lib/ds/DataStore');
 
-describe.only('resource', function() {
+describe('resource', function() {
   describe('ApiKey', function() {
-    var sandbox, cbSpy, apiKey, getResourceStub;
+    var clientApiKeySecret, sandbox, cbSpy, apiKey, getResourceStub;
 
     before(function(done) {
+      clientApiKeySecret = uuid();
       sandbox = sinon.sandbox.create();
       cbSpy = sandbox.spy();
 
@@ -23,10 +24,16 @@ describe.only('resource', function() {
         }
       });
 
-      var clientApiKeySecret = uuid();
-      apiKey.dataStore = new DataStore({client:{apiKey: {id: '1', secret: clientApiKeySecret}}});
+      apiKey.dataStore = new DataStore({
+        client: {
+          apiKey: {
+            id: '1',
+            secret: clientApiKeySecret
+          }
+        }
+      });
 
-      getResourceStub = sinon.stub(apiKey.dataStore,'getResource',function(){
+      getResourceStub = sinon.stub(apiKey.dataStore, 'getResource', function() {
         var args = Array.prototype.slice.call(arguments);
         var href = args.shift();
         var callback = args.pop();
@@ -36,7 +43,7 @@ describe.only('resource', function() {
       done();
     });
 
-    after(function () {
+    after(function() {
       sandbox.restore();
     });
 
@@ -46,14 +53,14 @@ describe.only('resource', function() {
 
     describe('getAccount', function() {
       it('should return an Account object', function(done) {
-        apiKey.getAccount(function(err, acc) {
+        apiKey.getAccount(function(err, account) {
           if (err) {
             return done(err);
           }
 
-          assert(acc.href === '/boom');
+          assert(account.href === '/boom');
           assert(getResourceStub.calledOnce);
-          
+
           done();
         });
       });
