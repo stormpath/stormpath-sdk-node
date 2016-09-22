27e1c99be4bc10956a20f473c41e476f336a477a
Luka Skukan <luka.skukan@infinum.hr>
2016-09-22 16:50:30 +0200

    Write tests for ResourceFactory resource expansion

--- test/sp.resource.resourceFactory_test.js
+++ test/sp.resource.resourceFactory_test.js
@@ -2,6 +2,9 @@ var common = require('./common');
 var _ = common._;
 
 var Tenant = require('../lib/resource/Tenant');
+var Account = require('../lib/resource/Account');
+var Group = require('../lib/resource/Group');
+var CustomData = require('../lib/resource/CustomData');
 var CollectionResource = require('../lib/resource/CollectionResource');
 
 var instantiate = require('../lib/resource/ResourceFactory').instantiate;
@@ -24,7 +27,32 @@ describe('Resources: ', function () {
       before(function () {
         data = {
           href: '',
-          items: [{href: ''}, {href: ''}],
+          items: [
+            {
+              href: '',
+              account: {
+                href: ''
+              },
+              group: {
+                href: ''
+              },
+              customData: {
+                href: ''
+              }
+            },
+            {
+              href: '',
+              account: {
+                href: ''
+              },
+              group: {
+                href: ''
+              },
+              customData: {
+                href: ''
+              }
+            }
+          ],
           offset: 0,
           limit: 5
         };
@@ -38,19 +66,78 @@ describe('Resources: ', function () {
           item.should.be.an.instanceof(Tenant);
         });
       });
+
+      describe('expand query resource initialization', function() {
+        it('should work for one expansion parameter', function() {
+          instantiate(Tenant, data, {expand: 'account'});
+
+          _.each(data.items, function(item) {
+            item.account.should.be.an.instanceof(Account);
+            item.group.should.not.be.an.instanceof(Group);
+          });
+        });
+
+        it('should work for a comma-delimited list of parameters', function() {
+          instantiate(Tenant, data, {expand: 'account,group'});
+
+          _.each(data.items, function(item) {
+            item.account.should.be.an.instanceof(Account);
+            item.group.should.be.an.instanceof(Group);
+          });
+        });
+
+        it('should expand the customData field even if not specified in expand (backwards compatibility)', function() {
+          instantiate(Tenant, data);
+
+          _.each(data.items, function(item) {
+            item.customData.should.be.an.instanceof(CustomData);
+          });
+        });
+      });
     });
+
 
     describe('if data is a resource', function(){
       var data;
 
       before(function () {
-        data = {href: ''};
+        data = {
+          href: '',
+          account: {
+            href: ''
+          },
+          group: {
+            href: ''
+          },
+          customData: {
+            href: ''
+          }
+        };
       });
 
       it('should return wrapped obj', function(){
         var coll = instantiate(Tenant, data);
 
         coll.should.be.an.instanceof(Tenant);
+      });
+
+      describe('expand query resource initialization', function() {
+        it('should work for one expansion parameter', function() {
+          var coll = instantiate(Tenant, data, {expand: 'account'});
+          coll.account.should.be.an.instanceof(Account);
+          coll.group.should.not.be.an.instanceof(Group);
+        });
+
+        it('should work for a comma-delimited list of parameters', function() {
+          var coll = instantiate(Tenant, data, {expand: 'account,group'});
+          coll.account.should.be.an.instanceof(Account);
+          coll.group.should.be.an.instanceof(Group);
+        });
+
+        it('should expand the customData field even if not specified in expand (backwards compatibility)', function() {
+          var coll = instantiate(Tenant, data);
+          coll.customData.should.be.an.instanceof(CustomData);
+        });
       });
     });
   });
