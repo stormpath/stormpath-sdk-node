6401d92d16c00def2459439c858ddbf99d8f99a9
Luka Skukan <luka.skukan@infinum.hr>
2016-10-19 21:56:00 +0200

    Integrate other two authenticators

--- test/it/oauth_stormpath_token_it.js
+++ test/it/oauth_stormpath_token_it.js
@@ -5,6 +5,11 @@ var common = require('../common');
 var helpers = require('./helpers');
 
 var assert = common.assert;
+var sinon = common.sinon;
+
+var njwt = require('njwt');
+
+var JwtAuthenticator = require('../../lib/jwt/jwt-authenticator');
 
 describe('OAuthStormpathTokenAuthenticator', function () {
   var account;
@@ -43,6 +48,12 @@ describe('OAuthStormpathTokenAuthenticator', function () {
 
     it('should return a OAuthStormpathTokenAuthenticator instance', function () {
       assert.instanceOf(instance, stormpath.OAuthStormpathTokenAuthenticator);
+    });
+
+    describe('inheritance', function() {
+      it('should inherit from ScopeFactoryAuthenticator', function() {
+        assert.equal(stormpath.OAuthStormpathTokenAuthenticator.super_.name, 'ScopeFactoryAuthenticator');
+      });
     });
 
     it('should return a new instance', function () {
@@ -156,6 +167,182 @@ describe('OAuthStormpathTokenAuthenticator', function () {
           assert.equal(err.developerMessage, 'token parameter is required; it cannot be null, empty, or blank.');
           assert.notOk(authenticationResult);
           done();
+        });
+      });
+    });
+
+    describe('scope factory properties', function() {
+      var auth;
+      var spy;
+      var scopeFactoryFunction;
+      var scope;
+      var signingKey;
+
+      before(function() {
+        auth = new stormpath.OAuthStormpathTokenAuthenticator(application);
+        spy = sinon.spy();
+        scopeFactoryFunction = function scopeFactoryFunction(authenticationResult, requestedScope, callback) {
+          spy(authenticationResult, requestedScope, callback);
+          callback(null, scope);
+        };
+
+        scope = 'test';
+        signingKey = application.dataStore.requestExecutor.options.client.apiKey.secret;
+
+        auth.setScopeFactory(scopeFactoryFunction);
+        auth.setScopeFactorySigningKey(signingKey);
+      });
+
+      after(function() {
+        // Unset the scope factory props
+        auth.setScopeFactory();
+        auth.setScopeFactorySigningKey();
+      });
+
+      it('should call the scope factory function if the scope is defined, with the correct `requestedScope`', function(done) {
+        auth.authenticate({stormpath_token: validToken, scope: scope}, function(err, result) {
+          if (err) {
+            return done(err);
+          }
+
+          /* jshint -W030 */
+          spy.should.have.been.calledOnce;
+          /* jshint +W030 */
+          spy.args[0][1].should.equal(scope);
+          assert.ok(result);
+          done();
+        });
+      });
+
+      it('should append a scope field from the factory to the result token, if the factory result is a truthy string', function(done) {
+        auth.authenticate({stormpath_token: validToken, scope: scope}, function(err, result) {
+          if (err) {
+            return done(err);
+          }
+
+          /* jshint -W030 */
+          spy.should.have.been.calledTwice;
+          /* jshint +W030 */
+
+          // Decoded token
+          assert.ok(result);
+          assert.ok(result.accessToken);
+          assert.ok(result.accessToken.body);
+          assert.equal(result.accessToken.body.scope, scope);
+
+          // Manual decoding check
+          njwt.verify(result.accessTokenResponse.access_token, signingKey, function(err, token) {
+            assert.ok(token);
+            assert.ok(token.body);
+            assert.equal(token.body.scope, scope);
+            done();
+          });
+        });
+      });
+
+      it('should leave all the other fields in the header and body of the JWT untouched', function(done) {
+        var bodyField;
+
+        auth.authenticate({stormpath_token: validToken, scope: scope}, function(err, authResponse) {
+          if (err) {
+            return done(err);
+          }
+
+          /* jshint -W030 */
+          spy.should.have.been.calledThrice;
+          /* jshint +W030 */
+
+          var originalToken = spy.args[2][0];
+          var token = authResponse.accessToken;
+
+          assert.deepEqual(originalToken.header, token.header);
+
+          for (bodyField in originalToken.body) {
+            if (originalToken.body.hasOwnProperty(bodyField)) {
+              assert.equal(originalToken.body[bodyField], token.body[bodyField]);
+            }
+          }
+
+          done();
+        });
+      });
+
+      describe('JwtAuthenticator validation', function() {
+        var jwtAuthenticator;
+        var token;
+
+        before(function(done) {
+          jwtAuthenticator = new JwtAuthenticator(application);
+          auth.authenticate({stormpath_token: validToken, scope: scope}, function(err, resp) {
+            if (err) {
+              return done(err);
+            }
+            token = resp.accessTokenResponse.access_token;
+            done();
+          });
+        });
+
+        describe('without local validation', function() {
+          it('should validate the token', function(done) {
+            jwtAuthenticator.authenticate(token, function(err, data) {
+              assert.notOk(err);
+              assert.ok(data);
+              done();
+            });
+          });
+
+          it('should yield a result containing the scope', function(done) {
+            jwtAuthenticator.authenticate(token, function(err, data) {
+              assert.ok(data.expandedJwt);
+              assert.ok(data.expandedJwt.claims);
+              assert.equal(data.expandedJwt.claims.scope, scope);
+              done();
+            });
+          });
+        });
+
+        describe('with local validation', function() {
+          before(function() {
+            jwtAuthenticator.withLocalValidation();
+          });
+
+          it('should validate the token', function(done) {
+            jwtAuthenticator.authenticate(token, function(err, data) {
+              assert.notOk(err);
+              assert.ok(data);
+              done();
+            });
+          });
+
+          it('should yield a result containing the scope', function(done) {
+            jwtAuthenticator.authenticate(token, function(err, data) {
+              assert.ok(data.expandedJwt);
+              assert.ok(data.expandedJwt.body);
+              assert.equal(data.expandedJwt.body.scope, scope);
+              done();
+            });
+          });
+        });
+      });
+
+      describe('errors', function() {
+
+        before(function() {
+          auth.setScopeFactorySigningKey();
+        });
+
+        after(function() {
+          auth.setScopeFactorySigningKey(signingKey);
+        });
+
+        it('should call the callback with an error if used with a factory function but without a key', function(done) {
+          var callback = function(err, data) {
+            assert.ok(err);
+            assert.notOk(data);
+            done();
+          };
+
+          auth.authenticate({stormpath_token: validToken, scope: scope}, callback);
         });
       });
     });
